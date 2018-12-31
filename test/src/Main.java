aaa86b63ccbe3ca03f406389d2156120b68abb91
JasonL <jasonl@fsbtech.com>
2018-12-31 18:03:31 +0000

    reflection test

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,6 +1,29 @@
+import java.lang.reflect.Field;
+
 public class Main {
+
+    public int i=10;
+
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+
+        Main main = new Main();
+        try
+        {
+            Field intField = main.getClass().getDeclaredField("i");
+            System.out.printf("Value of i: %d%n", intField.getInt(main));
+            main.i=50;
+            System.out.printf("Value of i: %d%n", intField.getInt(main));
+
+        } catch (NoSuchFieldException e)
+        {
+            e.printStackTrace();
+        } catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
 }
