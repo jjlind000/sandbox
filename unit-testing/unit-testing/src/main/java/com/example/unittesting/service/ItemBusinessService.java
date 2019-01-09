5c0c6ab3d6263cc29c38630d7eb6536b989b964b
JasonL <jasonl@fsbtech.com>
2019-01-09 15:40:23 +0000

    more stuff including gitignore

--- unit-testing/unit-testing/src/main/java/com/example/unittesting/service/ItemBusinessService.java
+++ unit-testing/unit-testing/src/main/java/com/example/unittesting/service/ItemBusinessService.java
@@ -2,16 +2,22 @@ package com.example.unittesting.service;
 
 import com.example.unittesting.data.ItemRepository;
 import com.example.unittesting.model.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+//import java.util.logging.Logger;
 
 @Component //needed if elsewhere an instance of ItemBusinessService is injected using Autowiring
 public class ItemBusinessService {
 
     @Autowired
     private ItemRepository itemRepository;
+
+    Logger log = LoggerFactory.getLogger("testlogger");
+
 
     public Item getHardCodedItem(){
         return new Item(1,"Dummy item", 9.99, 10);
@@ -23,5 +29,12 @@ public class ItemBusinessService {
             item.setValue(item.getPrice() * item.getQuantity());
         }
         return items;
+    }
+
+    public Item createItem(Item item){
+        log.info("creating item " + item);
+        Item savedItem=itemRepository.save(item);
+        savedItem.setValue(savedItem.getPrice()*savedItem.getQuantity()*10);
+        return savedItem;
     }
 }
